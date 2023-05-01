68705344965a216a169ea8369220953fb8335b0c
Jared Orler <orlerdev@icloud.com>
2023-05-01 12:51:28 -0500

    tables exercises and database exercises complete

--- albumns_migration.sql
+++ albumns_migration.sql
@@ -1,1 +1,13 @@
 USE codeup_test_db;
+
+DROP TABLE IF EXISTS albumns;
+
+CREATE TABLE albumns
+(
+    id     INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    artist VARCHAR(50) NOT NULL,
+    name   VARCHAR(50) NOT NULL,
+    release_date int NOT NULL,
+    sales FLOAT NOT NULL,
+    genre VARCHAR(50) NOT NULL
+);
